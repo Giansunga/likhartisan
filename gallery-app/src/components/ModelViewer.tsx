0ce0be98b123c591c6c71d2f4b1b4047316d0ebd
Giansunga <212896890+Giansunga@users.noreply.github.com>
2026-07-16 22:41:22 +0800

    Revert "fix: switch environment preset to apartment to remove vertical banding on pot interior"
    This reverts commit a75a63bf0e4619dcdccfdd609e40d96a7845fefc.

--- gallery-app/src/components/ModelViewer.tsx
+++ gallery-app/src/components/ModelViewer.tsx
@@ -65,7 +65,7 @@ export default function ModelViewer({ url }: { url: string }) {
         <directionalLight position={[5, 8, 5]} intensity={1.2} />
         <directionalLight position={[-5, 5, -5]} intensity={0.8} />
         <directionalLight position={[0, -2, 5]} intensity={0.4} />
-        <Environment preset="apartment" />
+        <Environment preset="city" />
 
         <Suspense fallback={null}>
           <FittedScene url={url} onCenter={setTarget} />
